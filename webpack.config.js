49ce868c5c29129136972c91806c63533be74a72
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2021-12-22 16:20:13 -0600

    initial commit, run start-dev to serve from public folder

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: ["./app/main.js"],
+  // this must MATCH the entry point where I'm using ReactDOM.render!
+  output: {
+    path: __dirname,
+    filename: "./bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react", "@babel/preset-env"],
+        },
+      },
+    ],
+  },
+};
