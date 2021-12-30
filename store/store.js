09a64e88a709cc9f548158fe84e9b3b47b9303c7
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2021-12-30 15:07:10 -0600

    add redux devtools extension to package and store

--- store/store.js
+++ store/store.js
@@ -1,12 +1,18 @@
 import { applyMiddleware, createStore } from "redux";
 
+import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
 import reducer from "./reducer";
 import thunkMiddleware from "redux-thunk";
 
+const MiddlewareWithDevtools = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger())
+);
+
 const store = createStore(
   reducer,
-  applyMiddleware(thunkMiddleware, createLogger())
+  MiddlewareWithDevtools
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
