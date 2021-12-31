319777aad2e60fbcf5382ea4cd5e14e39ba56365
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2021-12-31 15:34:18 -0600

    add react bootstrap

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import Button from "react-bootstrap/Button";
+import { Navigate } from "react-router-dom";
 
 const Home = () => {
   const { username } = useSelector((state) => state.auth);
   return (
     <div className="home">
       Welcome, <strong>{username}</strong>
+      <Button
+        variant="warning"
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </Button>
     </div>
   );
 };
--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 // magically inject CSS into the entire app:
 import "../public/index.css";
+import "bootstrap/dist/css/bootstrap.min.css"; //bootstrap CSS
 
 import App from "./App";
 import { Provider } from "react-redux";
