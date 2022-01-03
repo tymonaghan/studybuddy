92b0af9d397db8fed23582b14bdf872dba250df0
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-03 13:58:06 -0600

    trying heroku deployment again

--- start.js
+++ start.js
@@ -3,8 +3,8 @@ const app = require("./server");
 
 // listen for requests
 const port = process.env.PORT || 3030; // this can be very useful if you deploy to Heroku!
-// db.sync({ force: true }).then(() => {
-db.sync().then(() => {
+db.sync({ force: true }).then(() => {
+  // db.sync().then(() => {
   app.listen(port, () => {
     console.log(`Snake, tune your Codec to frequency ${port}`);
   });
