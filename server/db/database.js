1111367a9c6145392ded9d680d47038ba127a713
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-03 14:19:40 -0600

    update database to use SSL

--- server/db/database.js
+++ server/db/database.js
@@ -3,6 +3,10 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost:5432/studybuddy",
   {
+    ssl: true,
+    dialectOptions: {
+      ssl: true,
+    },
     logging: false,
   }
 );
