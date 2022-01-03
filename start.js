17622379db9d66ed3f415dde1e88f294010c9a61
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-03 14:15:30 -0600

    update start script to async function

--- start.js
+++ start.js
@@ -3,9 +3,18 @@ const app = require("./server");
 
 // listen for requests
 const port = process.env.PORT || 3030; // this can be very useful if you deploy to Heroku!
-db.sync({ force: true }).then(() => {
-  // db.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Snake, tune your Codec to frequency ${port}`);
-  });
-});
+
+const start = async () => {
+  try {
+    await db.sync({ force: true }).then(() => {
+      // db.sync().then(() => {
+      app.listen(port, () => {
+        console.log(`Snake, tune your Codec to frequency ${port}`);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
