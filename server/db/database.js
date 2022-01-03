d952be814234406d8e666ab3c0cb0ed6d2c792e7
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-03 14:39:30 -0600

    try adding dialectOptions to sequelize instance config

--- server/db/database.js
+++ server/db/database.js
@@ -3,7 +3,11 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost:5432/studybuddy",
   {
-    ssl: { rejectUnauthorized: false },
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
   }
 );
