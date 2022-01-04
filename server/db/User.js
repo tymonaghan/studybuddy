308542f269467b1658420a290e396cdbee3e8560
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-04 12:09:56 -0600

    add a couple VERY basic tests

--- server/db/User.js
+++ server/db/User.js
@@ -54,7 +54,7 @@ User.findByToken = async function (token) {
   // this takes a token as an argument. if valid, returns User instance object.
   try {
     console.log(`JWT is: `);
-    console.dir(process.env);
+    console.dir(process.env.JWT);
     const { id } = await jwt.verify(token, process.env.JWT || "atlantis");
     const user = User.findByPk(id);
     if (!user) {
---
+++ tests/startup_tests.js
@@ -0,0 +1,19 @@
+// copying and modifying from Pillars tests
+const { expect } = require("chai");
+
+// import { cyan } from "chalk";
+const { db, User } = require("../server/db");
+
+const _app = require("../server/index");
+const app = require("supertest")(_app);
+
+describe("Basic GET routes", () => {
+  it("allows connections to project", async () => {
+    const response = await app.get("/");
+    expect(response.status).to.equal(200);
+  });
+  it("/api/projects returns special message", async () => {
+    const response = await app.get("/api/projects");
+    expect(response.status).to.equal(418);
+  });
+});
