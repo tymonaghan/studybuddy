cdfc246138a6445d4a2a0e7ce93b7a2d5d0c007c
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-01-05 15:42:29 -0600

    add express route for adding new project

--- server/projects.js
+++ server/projects.js
@@ -9,6 +9,21 @@ router.get("/", (req, res, next) => {
   res.status(418).send("reached the /api/projects GET route, good job");
 });
 
+router.post("/addNew", (req, res, next) => {
+  try {
+    const newProject = Project.create({
+      name: req.body.project.name,
+      status: req.body.project.status,
+      userId: req.body.userId,
+    });
+    res.send(`hi from the add new project route.`);
+  } catch (error) {
+    console.log(
+      `error from the router.post /api/projects/addNew route: ${error}`
+    );
+  }
+});
+
 router.get("/countSources", async (req, res, next) => {
   // res.status(418).send(`you suck!`);
   // res.send(req.body);
