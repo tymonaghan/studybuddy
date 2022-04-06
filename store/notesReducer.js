3b2421aaf993455dc04f93e7a8e4b908abb8e21a
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2022-04-06 16:10:06 -0500

    add action and thunk to update note (in order to attach it to a claim)

--- store/notesReducer.js
+++ store/notesReducer.js
@@ -2,6 +2,7 @@ const SET_CURRENT_NOTES = "SET_CURRENT_NOTES";
 const ADD_NEW_NOTE = "ADD_NEW_NOTE";
 const GET_NOTES_BY_PROJECT = "GET_NOTES_BY_PROJECT";
 const DELETE_NOTE = "DELETE_NOTE";
+const UPDATE_NOTE = "UPDATE_NOTE";
 
 const Axios = require("axios");
 
@@ -14,13 +15,15 @@ const deleteNote = (noteId) => {
 };
 
 const setCurrentNotes = (notes) => {
-  // console.log(`hello from setCurrentNotes action creator. notes:`);
-  // console.dir(notes);
   return { type: SET_CURRENT_NOTES, notes };
 };
 
 const getAllTheNotes = (notes) => {
   return { type: GET_NOTES_BY_PROJECT, notes };
+};
+
+const updateNote = (noteId, note) => {
+  return { type: UPDATE_NOTE, noteId, note };
 };
 
 export const getNotesForProject = (projectId) => async (dispatch) => {
@@ -37,20 +40,12 @@ export const getNotesForProject = (projectId) => async (dispatch) => {
 
 export const addNewNoteToDb =
   (projectId, sourceId, newNote) => async (dispatch) => {
-    // console.log(
-    //   `gday from the thunk mate.\nprojectId:${projectId}\nsourceId:${sourceId}\nnewNote:${newNote}`
-    // );
     try {
       const response = await Axios({
         method: "post",
         url: `/api/projects/${projectId}/source/${sourceId}/addNote`,
         data: newNote,
       });
-      // console.log(
-      //   `data we got here from axios seems to beeee ${response.data}`
-      // );
-      // console.dir(response.data);
-
       dispatch(addNewNote(response.data));
     } catch (error) {
       console.log(`error in the addNewNoteToDb thunk: ${error}`);
@@ -68,6 +63,18 @@ export const setCurrentNotesThunk = (noteId) => async (dispatch) => {
   }
 };
 
+export const attachNoteToClaimInDb = (noteId, claimId) => async (dispatch) => {
+  try {
+    const response = await Axios({
+      method: "put",
+      url: `/api/projects/:projectId/note/${noteId}/attachToClaim/${claimId}`,
+    });
+    dispatch(updateNote(noteId, response.data));
+  } catch (error) {
+    console.log(`error in the attachNoteToClaimInDb thunk: ${error}`);
+  }
+};
+
 export function currentNotesReducer(state = [], action) {
   switch (action.type) {
     case SET_CURRENT_NOTES:
@@ -76,6 +83,11 @@ export function currentNotesReducer(state = [], action) {
       return [...action.notes];
     case ADD_NEW_NOTE:
       return [...state, action.note];
+    case UPDATE_NOTE:
+      return state.map((note) => {
+        if (note.id == action.noteId) return action.note;
+        else return note;
+      });
     default:
       return state;
   }
